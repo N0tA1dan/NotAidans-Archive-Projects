488f3b2cf4a9ca253f423ae85f5b139f3ed95916
Aidan <71911942+N0tA1dan@users.noreply.github.com>
2021-11-15 21:06:46 -0800

    Create uwucrypt-java-test.java

---
+++ uwucrypt-java-test.java
@@ -0,0 +1,16 @@
+
+import java.math.BigInteger;
+
+public class UwUcrypt {
+    public static void main(String[] args) {
+        String hello = "hello";
+        int hey = hello.length();
+
+        for(int loop_1 = 0; loop_1 < hey; loop_1++){
+            char owo = hello.charAt(loop_1);
+            int heyo = (int) owo;
+
+            System.out.print(heyo);
+        }
+    }
+}
